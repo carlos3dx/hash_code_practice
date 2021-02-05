1331c8ba897764b994bf39f71bd8ceb8ce3120df
Carlos Villar <carlosbetera3dx@gmail.com>
2021-02-05 23:40:11 +0100

    added test

--- unit_tests/test_population_generator.py
+++ unit_tests/test_population_generator.py
@@ -59,3 +59,36 @@ class TestPopulationGenerator:
             assert validator.validate(result)
             remaining_people = competition.total_people - sum([x[0] for x in result])
             assert not remaining_people
+
+    def test_generate_possible_valid_solutions_minimizing_waste_way_more_people_than_pizzas(self):
+        pizzas = {0: [0, 1, 2],
+                  1: [3, 4, 5],
+                  2: [3, 4, 5],
+                  3: [3, 4, 5],
+                  4: [3, 4, 5],
+                  5: [3, 4, 5],
+                  6: [6, 3, 1],
+                  7: [6, 3, 1],
+                  8: [6, 3, 1],
+                  9: [6, 3, 1],
+                  10: [4, 3, 5],
+                  11: [4, 3, 5],
+                  12: [4, 3, 5],
+                  13: [4, 3, 5],
+                  14: [6, 5]}
+        pizzeria = Pizzeria(pizzas, 7)
+        competition = Competition(40, 20, 10)
+        num_pizzas = len(pizzeria.pizzas)
+        validator = Validator(competition, num_pizzas)
+
+        num_results = 10
+
+        results = generate_pop(num_results, competition, pizzeria)
+
+        assert len(results) == num_results
+
+        for result in results:
+            assert validator.validate(result)
+            remaining_pizzas = num_pizzas - sum([len(x[1:]) for x in result])
+            remaining_people = competition.total_people - sum([x[0] for x in result])
+            assert remaining_people > remaining_pizzas
